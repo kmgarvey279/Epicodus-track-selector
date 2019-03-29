747b74761f05c2a57d0ff0cdbc576c2b408c115b
Kevin Garvey <kmgarvey279@gmail.com>
2019-03-29 13:40:31 -0700

    Added to scripts.js. It will now output the final else in a series of if statements, but will not yet take user values into account

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,11 @@
 $().ready(function() {
   $("form#trackQuestions").submit(function(event) {
-    event.preventDefault()
     var csharp = 0;
     var java = 0;
     var php = 0;
     var ruby = 0;
-    var result = "test here";
     if ($('input[type="question1"]:checked').val() === "A") {
-        end = "front";
+        result = "CSS and Design";
     } else {
       if ($('input[type="question2"]:checked').val() === "A") {
         ++csharp;
@@ -17,6 +15,7 @@ $().ready(function() {
       } else if ($('input[type="question2"]:checked').val() === "C") {
         ++ruby;
       }
+
       if ($('input[type="question3"]:checked').val() === "A") {
         ++csharp;
       } else if ($('input[type="question3"]:checked').val() === "B") {
@@ -26,6 +25,7 @@ $().ready(function() {
       } else if ($('input[type="question3"]:checked').val() === "D") {
         ++ruby;
       }
+
       if ($('input[type="question4"]:checked').val() === "A") {
         ++ruby;
       } else if ($('input[type="question4"]:checked').val() === "B") {
@@ -36,6 +36,7 @@ $().ready(function() {
       } else if ($('input[type="question4"]:checked').val() === "D") {
         ++ruby;
       }
+
       if ($('input[type="question5"]:checked').val() === "A") {
         ++java;
       } else if ($('input[type="question5"]:checked').val() === "B") {
@@ -44,18 +45,18 @@ $().ready(function() {
       } else if ($('input[type="question5"]:checked').val() === "C") {
         ++ruby;
       }
+
+      if (csharp > ruby && csharp > java && csharp > php) {
+        result = "C#";
+      } else if (ruby > java && ruby > php) {
+        result = "Ruby";
+      } else if (java > php) {
+        Result = "Java"
+      } else {
+        result = "PHP";
+      }
     }
-    if end === "front" {
-      result = "CSS and Design"
-    } else if (csharp > ruby && csharp > java && csharp > php) {
-      result = "C#";
-    } else if (ruby > java && ruby > php) {
-      result = "Ruby";
-    } else if (java > php) {
-      Result = "Java"
-    } else {
-      result = "PHP";
-    }
-    $("#language").empty().append(result);
-  }};
-}};
+      $("#language").empty().append(result);
+      event.preventDefault();
+  });
+});
