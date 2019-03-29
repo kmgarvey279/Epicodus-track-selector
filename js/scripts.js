bf460410b892c410319686802ac89a2cdb58521f
Kevin Garvey <kmgarvey279@gmail.com>
2019-03-29 16:19:25 -0700

    add ability to evaluate choice and return suggested track in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,65 @@
 $().ready(function() {
-  $("form#trackQuestions").submit(function(event) {
+  $("#trackQuestions").submit(function() {
+    event.preventDefault();
+    var result;
+    var answer1 = $('input:radio[name="question1"]:checked').val()
+    var answer2 = $('input:radio[name="question2"]:checked').val()
+    var answer3 = $('input:radio[name="question3"]:checked').val()
+    var answer4 = $('input:radio[name="question4"]:checked').val()
+    var answer5 = $('input:radio[name="question5"]:checked').val()
     var csharp = 0;
     var java = 0;
-    var php = 0;
     var ruby = 0;
-    if ($('input[type="question1"]:checked').val() === "A") {
-        result = "CSS and Design";
-    } else {
-      if ($('input[type="question2"]:checked').val() === "A") {
-        ++csharp;
-        ++java;
-      } else if ($('input[type="question2"]:checked').val() === "B") {
-        ++php;
-      } else if ($('input[type="question2"]:checked').val() === "C") {
-        ++ruby;
-      }
+    var php = 0;
 
-      if ($('input[type="question3"]:checked').val() === "A") {
-        ++csharp;
-      } else if ($('input[type="question3"]:checked').val() === "B") {
-        ++java;
-      } else if ($('input[type="question3"]:checked').val() === "C") {
-        ++php;
-      } else if ($('input[type="question3"]:checked').val() === "D") {
-        ++ruby;
-      }
-
-      if ($('input[type="question4"]:checked').val() === "A") {
-        ++ruby;
-      } else if ($('input[type="question4"]:checked').val() === "B") {
-        ++php;
-      } else if ($('input[type="question4"]:checked').val() === "C") {
-        ++csharp;
-        ++java;
-      } else if ($('input[type="question4"]:checked').val() === "D") {
-        ++ruby;
-      }
-
-      if ($('input[type="question5"]:checked').val() === "A") {
-        ++java;
-      } else if ($('input[type="question5"]:checked').val() === "B") {
-        ++php;
-        ++csharp;
-      } else if ($('input[type="question5"]:checked').val() === "C") {
-        ++ruby;
-      }
-
-      if (csharp > ruby && csharp > java && csharp > php) {
-        result = "C#";
-      } else if (ruby > java && ruby > php) {
-        result = "Ruby";
-      } else if (java > php) {
-        Result = "Java"
-      } else {
-        result = "PHP";
-      }
+    if (answer2 === "A") {
+      ++java;
+      ++csharp;
+    } else if (answer2 === "B") {
+      ++php;
+    } else if (answer2 === "C") {
+      ++ruby;
     }
-      $("#language").empty().append(result);
-      event.preventDefault();
+
+    if (answer3 === "A") {
+      ++csharp;
+    } else if (answer3 === "B") {
+      ++java;
+    } else if (answer3 === "C") {
+      ++php;
+    } else if (answer3 === "D") {
+      ++ruby;
+    }
+
+    if (answer4 === "A") {
+      ++ruby;
+    } else if (answer4 === "B") {
+      ++php;
+    } else if (answer4 === "C") {
+      ++csharp;
+      ++java;
+    }
+
+    if (answer4 === "A") {
+      ++java;
+    } else if (answer4 === "B") {
+      ++php;
+      ++csharp;
+    } else if (answer4 === "C") {
+      ++ruby;
+    }
+
+    if (answer1 === "A") {
+      result = "CSS and Design";
+    } else if (csharp > ruby && csharp > java && csharp > php) {
+      result = "C#";
+    } else if (ruby > java && ruby > php) {
+      result = "Ruby";
+    } else if (java > php) {
+      result = "Java"
+    } else {
+      result = "PHP";
+    }
+    $("#language").empty().append(result);
   });
 });
